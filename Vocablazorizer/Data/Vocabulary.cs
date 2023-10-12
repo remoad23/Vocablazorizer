d514097d51522e963cc695ef09f19dea942f2320
remoad23 <patrick24p@gmail.com>
2023-10-12 19:15:48 +0200

    Add project files.

---
+++ Vocablazorizer/Data/Vocabulary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocablazorizer.Data
+{
+    public class Vocabulary
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string WordTranslated { get; set; }
+
+
+    }
+}
---
+++ Vocablazorizer/Services/VocabularyService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vocablazorizer.Data;
+
+namespace Vocablazorizer.Services
+{
+    public class VocabularyService
+    {
+        private List<Vocabulary> VocabularyList;
+
+        public VocabularyService() 
+        {
+            VocabularyList = LoadVocabulariesFromFile();
+        }
+
+        public List<Vocabulary> GetVocabularies()
+        {
+            List<Vocabulary> vocabularies = new();
+
+
+
+            return vocabularies;
+        }
+
+        public void AddNewVocabularies(Vocabulary vocabulary)
+        {
+            VocabularyList.Add(vocabulary);
+            SaveVocabulariesFromFile();
+        }
+
+        public void DeleteVocabulary(Vocabulary vocabulary)
+        {
+            VocabularyList.Remove(vocabulary);
+            SaveVocabulariesFromFile();
+        }
+
+        // maybe use databinding instead?
+        public void UpdateVocabulary(Vocabulary vocabulary,string word,string wordToTranslate)
+        {
+            Vocabulary vocabularyToUpdate = VocabularyList.Where(voc => voc.Id == vocabulary.Id).FirstOrDefault();
+            vocabularyToUpdate.Word = word;
+            vocabularyToUpdate.WordTranslated = wordToTranslate;
+        }
+
+        internal void SaveVocabulariesFromFile()
+        {
+
+        }
+
+        internal List<Vocabulary> LoadVocabulariesFromFile()
+        {
+            List<Vocabulary> vocabularies = new();
+
+
+
+            return vocabularies;
+        }
+    }
+}
